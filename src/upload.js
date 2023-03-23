56836f2b0c5c5812ac0644840a15dc3f175e20f4
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2023-03-23 16:59:19 +0530

    add files

--- src/upload.js
+++ src/upload.js
@@ -2,7 +2,7 @@
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3();
 
-const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+const BUCKET_NAME = 'vishalsaw';
 
 module.exports.handler = async (event) => {
     console.log(event);
