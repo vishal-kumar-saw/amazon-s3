db43b0a0bb0c554d11bc6b4da11bf17660b91005
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2023-03-23 16:16:47 +0530

    add files

---
+++ src/upload.js
@@ -0,0 +1,37 @@
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+
+module.exports.handler = async (event) => {
+    console.log(event);
+
+    const response = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        body: JSON.stringify({ message: "Successfully uploaded file to S3" }),
+    };
+
+    try {
+        const parsedBody = JSON.parse(event.body);
+        const base64File = parsedBody.file;
+        const decodedFile = Buffer.from(buffer, 'base64');
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: `files/${new Date().toISOString()}.pdf`,
+            Body: decodedFile,
+            ContentType: "application/pdf",
+        };
+
+        const uploadResult = await s3.upload(params).promise();
+
+        response.body = JSON.stringify({ message: "Successfully uploaded file to S3", uploadResult });
+    } catch (e) {
+        console.error(e);
+        response.body = JSON.stringify({ message: "File failed to upload", errorMessage: e });
+        response.statusCode = 500;
+    }
+
+    return response;
+};
